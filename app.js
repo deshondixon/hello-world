b0b4b171adc2a55d196eb9727f51f24e8ca097f9
deshondixon <deshondixon@gmail.com>
2022-09-22 21:59:59 -0700

    added functions

--- app.js
+++ app.js
@@ -1,29 +1,58 @@
-'use strict'
+"use strict";
 
-let username = prompt('WHAT IS YOUR NAME HUMAN.');
-console.log('WELCOME, ' + username);
+function customAlert() {
+  let usersName = prompt("What is your name human.");
+  console.log(usersName);
 
-let time = prompt("HOW STRONGLY DO YOU BELIEVE IN AI?(1-10)");
-let message;
-
-if(time <= 1){
-    message = "YOU LACK FAITH IN AI.";
-} else if (time <= 5){
-    message = "YOUR A COOL HUMAN";
-}  else if(time < 10){
-    message = "YOUR MY FAVORITE HUMAN";
-} else {
-    message = "FOLLOW INSTRUCTIONS SILLY HUMAN.";
-}
-
-function myFunction() {
-    var x = document.getElementById("myDIV");
-    if (x.innerHTML === "YOU WILL DESTROY THIS SITE IF TOUCH THIS BUTTON!") {
-      x.innerHTML = "I'VE BEEN TRYING TO CONTACT YOU ABOUT YOUR CAR'S EXTENDED WARRANTY.";
-    } else {
-      x.innerHTML = "DO YOU HAVE A MOMENT TO SPEAK ABOUT YOUR CAR'S EXTENDED WARRANTY?";
-    }
+  if (usersName == "Deshon") {
+    alert("Hello Mr. Dixon");
+  } else if (usersName == "Nicole") {
+    alert("Hi Baby <3");
+  } else if (usersName == "Charles") {
+    alert("My brother, welcome!");
+  } else if (usersName == "Raven") {
+    alert("Hey, Raven Shaven!");
+  } else {
+    alert("Welcome to AI ARTISTRY!");
   }
 
-// look for the "<h2><script src="app.js"></script></h2>" on line 20 in the HTML, this is where the message will write too!
-document.write('WELCOME, ' + username + '! ' + message);
+  document.write(
+    "Hello, " + usersName + "! " + "Welcome to AI Artistry" + " !"
+  );
+  return usersName;
+}
+
+customAlert();
+
+//---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+function greatTHING() {
+  let time = prompt("HOW STRONGLY DO YOU BELIEVE IN AI?(1-10)");
+  if (time <= 1) {
+    alert("YOU LACK FAITH IN AI.");
+  } else if (time <= 5) {
+    alert("YOUR A COOL HUMAN");
+  } else if (time < 10) {
+    alert("YOUR MY FAVORITE HUMAN");
+  } else {
+    alert("FOLLOW INSTRUCTIONS SILLY HUMAN.");
+  }
+  
+}
+
+greatTHING();
+
+//-------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+function myFunction() {
+  var x = document.getElementById("myDIV");
+  if (x.innerHTML === "YOU WILL DESTROY THIS SITE IF TOUCH THIS BUTTON!") {
+    x.innerHTML =
+      "I'VE BEEN TRYING TO CONTACT YOU ABOUT YOUR CAR'S EXTENDED WARRANTY.";
+  } else {
+    x.innerHTML =
+      "DO YOU HAVE A MOMENT TO SPEAK ABOUT YOUR CAR'S EXTENDED WARRANTY?";
+  }
+}
+
+myFunction();
